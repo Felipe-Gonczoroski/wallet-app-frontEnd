dc38a9838f9784f73f7e06db1d2481b17755bd90
Felipe-Gonczoroski <atendimentofelipeneves@gmail.com>
2025-04-10 13:37:15 -0300

    feat: implement render data on table

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,3 +1,54 @@
+const renderFinancesList = (data) => {
+  const table = document.getElementById("finances-table");
+
+  data.map((item) => {
+    const tableRow = document.createElement("tr");
+    tableRow.className = "mt smaller";
+
+    //title  |  título
+    const titleTd = document.createElement("td");
+    const titleText = document.createTextNode(item.title);
+    titleTd.appendChild(titleText);
+    tableRow.appendChild(titleTd);
+
+    //category  |  categoria
+    const categoryTd = document.createElement("td");
+    const categoryText = document.createTextNode(item.name);
+    categoryTd.appendChild(categoryText);
+    tableRow.appendChild(categoryTd);
+
+    //date  |  data
+    const dateTd = document.createElement("td");
+    const dateText = document.createTextNode(
+      new Date(item.date).toLocaleDateString()
+    );
+    dateTd.appendChild(dateText);
+    tableRow.appendChild(dateTd);
+
+    //value  |  valor
+    const valueTd = document.createElement("td");
+    valueTd.className = "center";
+    const valueText = document.createTextNode(
+      new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      }).format(item.value)
+    );
+    valueTd.appendChild(valueText);
+    tableRow.appendChild(valueTd);
+
+    //delete  |  deletar
+    const deleteTd = document.createElement("td");
+    deleteTd.className = "right";
+    const deleteText = document.createTextNode("Deletar");
+    deleteTd.appendChild(deleteText);
+    tableRow.appendChild(deleteTd);
+
+    //table add tableRow  |  tabela adiciona linhas nela
+    table.appendChild(tableRow);
+  });
+};
+
 const renderFinanceElement = (data) => {
   const totalItems = data.length;
   const revenues = data
@@ -76,6 +127,7 @@ const onLoadFinancesData = async () => {
     );
     const data = await result.json();
     renderFinanceElement(data);
+    renderFinancesList(data);
     return data;
   } catch (error) {
     return { error };
